ebf5920280f0e76be30795282c73d10deac2c749
swastikCommits <textswastik.alt@gmail.com>
2025-07-29 22:35:54 +0530

    added Session Provider for auth

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import "@repo/ui/styles.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
 
 const geist = Geist({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={geist.className}>{children}</body>
+      <body className={geist.className}>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
