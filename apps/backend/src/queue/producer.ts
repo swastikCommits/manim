0648ec87414119c95ee4b36b5ba4413c809a89ca
swastikCommits <textswastik.alt@gmail.com>
2025-08-01 21:47:59 +0530

    added bullmq producer

---
+++ apps/backend/src/queue/producer.ts
@@ -0,0 +1,8 @@
+import { Queue } from "bullmq";
+import { connection } from "./redisClient"; 
+
+export const videoRenderQueue = new Queue("video-render", { connection });
+
+export function setupBullQueue() {
+  console.log("BullMQ Producer ready.");
+}
